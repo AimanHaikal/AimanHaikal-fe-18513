b286cd148963df1ab49f8ae9a9af4aa98ad1ed40
Aiman Haikal <aimanhaikal013@gmail.com>
2024-05-23 17:35:02 +0800

    Update Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 // app/layout.tsx
 import "./styles/globals.css";
-import { SlMagnifier } from "react-icons/sl";
+import { SlMagnifier, SlEnvolope, SlPhone,SlArrowRight  } from "react-icons/sl";
 import { VscAccount } from "react-icons/vsc";
+import { TfiLocationPin } from "react-icons/tfi";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -53,8 +54,89 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             <div className="user-name">John Glich</div>
           </div>
         </header>
-        <main className="main-content">{children}</main>
-        <footer>© 2024 Web Assessment</footer>
+        <div className="movie-gallery">
+          <h2>Find your movies here!</h2>
+          <p>
+            Explore our gallery full of exciting films from all around the globe
+          </p>
+          <p>Only your entertainment. No hidden charges or disturbing ads.</p>
+        </div>
+        <footer>
+          <div className="footer-on-left">
+            <h1>PcariMovie</h1>
+            <p className="footer-description">
+              Lorem ipsum dolor sit amet, consec tetur adipis cing elit, sed do
+              eiusmod tempor incididunt ut labore et.
+            </p>
+            <br />
+            <p>Join Newsletters</p>
+            <div className="newsletter-container">
+            <input 
+    type="email" 
+    placeholder="Insert your mail here" 
+    className="newsletter-input" 
+  />
+  <button className="newsletter-button"><SlArrowRight/> </button></div>
+          </div>
+          <div className="footer-on-right">
+            <div className="footer-container">
+              <div className="footer-left">
+                <div className="footer-link-item">
+                  <a className="footer-title-item">Product</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Movies</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">TV Show</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Video</a>
+                </div>
+              </div>
+              <div className="footer-left">
+                <div className="footer-link-item">
+                  <a className="footer-title-item">Media Group</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Nice Studio</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Nice News</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Nice TV</a>
+                </div>
+              </div>
+              <div className="footer-left">
+                <div className="footer-link-item">
+                  <a className="footer-title-item">Sitemap</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">About</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Careers</a>
+                </div>
+                <div className="footer-link-item">
+                  <a href="#">Press</a>
+                </div>
+              </div>
+            </div>
+            <div className="footer-container-bottom">
+              <p>
+                <TfiLocationPin color="white" /> 8819 Ohio St. South Gate,
+                California 90280
+              </p>
+              <p>
+                <SlEnvolope color="white" /> ourstudio@hello.com
+              </p>
+              <p>
+                <SlPhone color="white" /> +271 386-647-3637
+              </p>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
