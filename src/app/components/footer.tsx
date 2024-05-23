3ab33fc8567d01b5432af276cec27c03b345aa03
Aiman Haikal <aimanhaikal013@gmail.com>
2024-05-23 20:45:22 +0800

    Update Footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,89 @@
-// components/Footer.tsx
-const Footer = () => (
-    <footer className="footer">
-      <p>© 2024 Web Assessment</p>
+// footer.tsx
+import { SlEnvolope, SlPhone, SlArrowRight } from "react-icons/sl";
+import { TfiLocationPin } from "react-icons/tfi";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-on-left">
+        <h1>PcariMovie</h1>
+        <p className="footer-description">
+          Lorem ipsum dolor sit amet, consec tetur adipis cing elit, sed do
+          eiusmod tempor incididunt ut labore et.
+        </p>
+        <br />
+        <p>Join Newsletters</p>
+        <div className="newsletter-container">
+          <input
+            type="email"
+            placeholder="Insert your mail here"
+            className="newsletter-input"
+          />
+          <button className="newsletter-button">
+            <SlArrowRight />{" "}
+          </button>
+        </div>
+      </div>
+      <div className="footer-on-right">
+        <div className="footer-container">
+          <div className="footer-left">
+            <div className="footer-link-item">
+              <a className="footer-title-item">Product</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Movies</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">TV Show</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Video</a>
+            </div>
+          </div>
+          <div className="footer-left">
+            <div className="footer-link-item">
+              <a className="footer-title-item">Media Group</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Nice Studio</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Nice News</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Nice TV</a>
+            </div>
+          </div>
+          <div className="footer-left">
+            <div className="footer-link-item">
+              <a className="footer-title-item">Sitemap</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">About</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Careers</a>
+            </div>
+            <div className="footer-link-item">
+              <a href="#">Press</a>
+            </div>
+          </div>
+        </div>
+        <div className="footer-container-bottom">
+          <p>
+            <TfiLocationPin color="white" /> 8819 Ohio St. South Gate,
+            California 90280
+          </p>
+          <p>
+            <SlEnvolope color="white" /> ourstudio@hello.com
+          </p>
+          <p>
+            <SlPhone color="white" /> +271 386-647-3637
+          </p>
+        </div>
+      </div>
     </footer>
-  )
-  
-  export default Footer
-  
+  );
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,10 @@
 "use client";
 
 import "./styles/globals.css";
-import { SlEnvolope, SlPhone, SlArrowRight } from "react-icons/sl";
-import { TfiLocationPin } from "react-icons/tfi";
 import MovieCard from "./components/movieCard";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
@@ -43,20 +41,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       viewCount: 21.3,
     },
   ];
-
-  useEffect(() => {
-    // Check if window is defined (running on the client-side)
-    if (typeof window !== "undefined") {
-      const path = window.location.pathname;
-      const navLinks = document.querySelectorAll(".nav-link");
-      navLinks.forEach((link) => {
-        if ((link as HTMLAnchorElement).getAttribute("href") === path) {
-          link.parentElement!.classList.add("active");
-        }
-      });
-    }
-  }, []); // Empty dependency array ensures this effect runs only once, similar to componentDidMount
-
+  
   return (
     <html lang="en">
       <body>
@@ -100,85 +85,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </div>
 
-        <footer>
-          <div className="footer-on-left">
-            <h1>PcariMovie</h1>
-            <p className="footer-description">
-              Lorem ipsum dolor sit amet, consec tetur adipis cing elit, sed do
-              eiusmod tempor incididunt ut labore et.
-            </p>
-            <br />
-            <p>Join Newsletters</p>
-            <div className="newsletter-container">
-              <input
-                type="email"
-                placeholder="Insert your mail here"
-                className="newsletter-input"
-              />
-              <button className="newsletter-button">
-                <SlArrowRight />{" "}
-              </button>
-            </div>
-          </div>
-          <div className="footer-on-right">
-            <div className="footer-container">
-              <div className="footer-left">
-                <div className="footer-link-item">
-                  <a className="footer-title-item">Product</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Movies</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">TV Show</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Video</a>
-                </div>
-              </div>
-              <div className="footer-left">
-                <div className="footer-link-item">
-                  <a className="footer-title-item">Media Group</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Nice Studio</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Nice News</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Nice TV</a>
-                </div>
-              </div>
-              <div className="footer-left">
-                <div className="footer-link-item">
-                  <a className="footer-title-item">Sitemap</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">About</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Careers</a>
-                </div>
-                <div className="footer-link-item">
-                  <a href="#">Press</a>
-                </div>
-              </div>
-            </div>
-            <div className="footer-container-bottom">
-              <p>
-                <TfiLocationPin color="white" /> 8819 Ohio St. South Gate,
-                California 90280
-              </p>
-              <p>
-                <SlEnvolope color="white" /> ourstudio@hello.com
-              </p>
-              <p>
-                <SlPhone color="white" /> +271 386-647-3637
-              </p>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
