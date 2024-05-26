502a3a8f9b3912d12a149d64d88aa17886d25a08
Aiman Haikal <aimanhaikal013@gmail.com>
2024-05-26 18:30:14 +0800

    Add GET Specific Movie Theater API

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,30 +1,33 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     // Add any other headers you need, such as authorization headers
   },
 });
 
 // Define your API endpoints
 const apiEndpoints = {
-  getMovies: () => api.get("/movies"),
-  getMovieById: (movieId: number) => api.get(`/movies/${movieId}`),
-  getMovieByGenre: (genre: string) => api.get(`/movies/genre/${genre}`),
-  getMovieByTimeSlot: (
-    theaterName: string,
-    timeStart: string,
-    timeEnd: string
-  ) =>
+  getMovies: () => api.get('/movies'),
+  getMovieById: (movieId: any) => api.get(`/movies/${movieId}`),
+  getMovieByGenre: (genre: any) => api.get(`/movies/genre/${genre}`),
+  getMovieByTimeSlot: (theaterName: any, timeStart: any, timeEnd: any) =>
     api.get(`/movies/timeslot`, {
       params: {
         theater_name: theaterName,
         time_start: timeStart,
         time_end: timeEnd,
+      },
+    }),
+  getMoviesByTheaterAndDate: (theaterName: any, dDate: any) =>
+    api.get(`/movies/theater-movies`, {
+      params: {
+        theater_name: theaterName,
+        d_date: dDate,
       },
     }),
   // Add more endpoints as needed
